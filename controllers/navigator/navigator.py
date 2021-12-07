101446a21d8d4bca9723dce3f1e4c04d59292683
patrikas-sestokas <arcal1359@gmail.com>
2021-12-07 21:55:28 +0200

    Implemented tile tracking logic, refactored code.

--- controllers/navigator/navigator.py
+++ controllers/navigator/navigator.py
@@ -4,76 +4,125 @@
 #  from controller import Robot, Motor, DistanceSensor
 from controller import Supervisor
 
-def run_robot(robot):
 
-    def is_finish_line(robot_vec, finish_vec):
-        return robot_vec[0] < finish_vec[0] + 0.1 and robot_vec[0] > finish_vec[0] - 0.1 and robot_vec[2] < finish_vec[2] + 0.1 and robot_vec[2] > finish_vec[2] - 0.1
-    
-    timestep = int(robot.getBasicTimeStep())
-    max_speed = 6.28
-    
-    left_motor = robot.getMotor('left wheel motor')
-    right_motor = robot.getMotor('right wheel motor')
-    
+class Tile:
+    x: int
+    y: int
+    tile_size = 0.25
+
+    def __init__(self, robot_pos):
+        self.x = robot_pos[0] // self.tile_size
+        self.y = robot_pos[2] // self.tile_size
+
+    def __eq__(self, other):
+        if not isinstance(other, Tile):
+            return False
+        return other.x == self.x and other.y == self.y
+
+    def __repr__(self):
+        return f'{{x: {self.x}, y: {self.y}}}'
+
+    def as_coordinates(self):
+        return self.x * 4 + self.tile_size / 2, self.y * 4 + self.tile_size / 2
+
+
+def setup_sensors(robot, time_step):
+    left_motor = robot.getDevice('left wheel motor')
     left_motor.setPosition(float('inf'))
-    left_motor.setVelocity(0.0)
-    
+    left_motor.setVelocity(0)
+
+    right_motor = robot.getDevice('right wheel motor')
     right_motor.setPosition(float('inf'))
     right_motor.setVelocity(0.0)
-    
-    gps = robot.getGPS('gps')
+
+    gps = robot.getDevice('gps')
     gps.enable(1)
-    
+    proximity_sensors = [robot.getDevice(f'ps{idx}') for idx in range(8)]
+    for sensor in proximity_sensors:
+        sensor.enable(time_step)
+    return [left_motor, right_motor, gps, proximity_sensors]
+
+
+def is_finish_line(robot_vec, finish_vec):
+    return finish_vec[0] + 0.1 > robot_vec[0] > finish_vec[0] - 0.1 and finish_vec[2] + 0.1 > robot_vec[2] > \
+           finish_vec[2] - 0.1
+
+
+def mode_1(proximity_sensors, max_speed):
+    left_wall = proximity_sensors[5].getValue() > 80
+    left_corner = proximity_sensors[6].getValue() > 80
+    front_wall = proximity_sensors[7].getValue() > 80
+
+    left_speed = max_speed
+    right_speed = max_speed
+
+    if front_wall:
+        # print("Turn right")
+        left_speed = max_speed
+        right_speed = -max_speed
+    else:
+        if left_wall:
+            # print("Drive forward")
+            left_speed = max_speed
+            right_speed = max_speed
+        else:
+            # print("Turn left")
+            left_speed = max_speed / 8
+            right_speed = max_speed
+        if left_corner:
+            # print("Too close, right")
+            left_speed = max_speed
+            right_speed = max_speed / 8
+
+    return left_speed, right_speed
+
+
+def run_robot(robot):
+    timeStep = int(robot.getBasicTimeStep())
+    max_speed = 6.28
+
+    left_motor, right_motor, gps, proximity_sensors = setup_sensors(robot, timeStep)
+
     finish_line_node = robot.getFromDef("finish_line")
     finish_line_translation = finish_line_node.getField("translation")
     finish_line_vector = finish_line_translation.getSFVec3f()
-    
-    prox_sensors = []
-    for ind in range(8):
-        sensor_name = 'ps' + str(ind)
-        prox_sensors.append(robot.getDistanceSensor(sensor_name))
-        prox_sensors[ind].enable(timestep)
-    
+
+    path = []
+
+    mode = 1
+
     # Main loop:
-    # - perform simulation steps until Webots is stopping the controller
-    while robot.step(timestep) != -1:
+    while robot.step(timeStep) != -1:
         robot_pos = gps.getValues()
         if is_finish_line(robot_pos, finish_line_vector):
-            max_speed = 0
+            left_motor.setVelocity(0)
+            right_motor.setVelocity(0)
+            mode = 2
+
+        tile = Tile(robot_pos)
+
+        if mode == 1:
+            if len(path) == 0 or path[-1] != tile:
+                if tile in path:
+                    path = path[:path.index(tile) + 1]
+                else:
+                    path.append(tile)
+                print(path)
+            left_speed, right_speed = mode_1(proximity_sensors, max_speed)
+
+        elif mode == 2:
             print("LABYRINTH SOLVED!")
-            
-        for ind in range(8):
-            pass#print(f"ind: {ind}, val: {prox_sensors[ind].getValue()}")
-            
-        left_wall = prox_sensors[5].getValue() > 80
-        left_corner = prox_sensors[6].getValue() > 80
-        front_wall = prox_sensors[7].getValue() > 80
-        
-        left_speed = max_speed
-        right_speed = max_speed
-        
-        if front_wall:
-            #print("Turn right")
-            left_speed = max_speed
-            right_speed = -max_speed
+            left_motor.setVelocity(0)
+            right_motor.setVelocity(0)
+            break
+
         else:
-            if left_wall:
-                #print("Drive forward")
-                left_speed = max_speed
-                right_speed = max_speed
-            else:
-                #print("Turn left")
-                left_speed = max_speed / 8
-                right_speed = max_speed
-            if left_corner:
-                #print("Too close, right")
-                left_speed = max_speed
-                right_speed = max_speed / 8
-            
+            raise Exception(f'Unknown mode {mode}')
+
         left_motor.setVelocity(left_speed)
         right_motor.setVelocity(right_speed)
 
-# Enter here exit cleanup code.
+
 if __name__ == "__main__":
     my_robot = Supervisor()
     run_robot(my_robot)

e3171c682c67f155ae80e2214731c09a77f66c1b
unknown <edgaras.stasiukaitis@gmail.com>
2021-12-08 15:18:56 +0200

    Partially working robot's relative angle calculations

--- controllers/navigator/navigator.py
+++ controllers/navigator/navigator.py
@@ -3,8 +3,8 @@
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
 from controller import Supervisor
-import math
 from controller import MouseState
+import math
 
 
 class Tile:
@@ -45,12 +45,16 @@ def setup_sensors(robot, time_step):
     pen.write(True)
 
     mouse = robot.getMouse()
-    mouse.enable(100)
+    mouse.enable(250)
+    mouse.enable3dPosition()
+
+    compass = robot.getDevice('compass')
+    compass.enable(1)
 
     proximity_sensors = [robot.getDevice(f'ps{idx}') for idx in range(8)]
     for sensor in proximity_sensors:
         sensor.enable(time_step)
-    return [left_motor, right_motor, gps, pen, mouse, proximity_sensors]
+    return [left_motor, right_motor, gps, pen, mouse, compass, proximity_sensors]
 
 
 def is_finish_line(robot_vec, finish_vec):
@@ -87,11 +91,27 @@ def mode_1(proximity_sensors, max_speed):
     return left_speed, right_speed
 
 
+def get_bearing_in_degrees(compass):
+    north = compass.getValues()
+    rad = math.atan2(north[0], north[2])
+    bearing = (rad - 1.5708) / 3.14 * 180.0
+    if bearing < 0.0:
+        bearing = bearing + 360.0
+    return bearing
+
+
+def get_absolute_angle_in_degrees(pointA, pointB):
+    dx = pointB[0] - pointA[0]  # path[-1].as_coordinates()[0]
+    dz = pointB[1] - pointA[2]  # path[-1].as_coordinates()[1]
+    absolute_angle = math.degrees(math.atan2(dz, dx))
+    return absolute_angle % 360
+
+
 def run_robot(robot):
     timeStep = int(robot.getBasicTimeStep())
-    max_speed = 6.28
+    max_speed = 0
 
-    left_motor, right_motor, gps, pen, mouse, proximity_sensors = setup_sensors(robot, timeStep)
+    left_motor, right_motor, gps, pen, mouse, compass, proximity_sensors = setup_sensors(robot, timeStep)
 
     finish_line_node = robot.getFromDef("finish_line")
     finish_line_translation = finish_line_node.getField("translation")
@@ -117,7 +137,7 @@ def run_robot(robot):
                     path = path[:path.index(tile) + 1]
                 else:
                     path.append(tile)
-                #print(path)
+                # print(path)
             left_speed, right_speed = mode_1(proximity_sensors, max_speed)
 
         elif mode == 2:
@@ -131,9 +151,10 @@ def run_robot(robot):
 
         point = (mouse.getState().x, mouse.getState().z)
         if not math.isnan(point[0]):
-            dx = point[0] - path[0].as_coordinates()[0]
-            dz = point[1] - path[0].as_coordinates()[1]
-            print(math.degrees(math.atan2(dz, dx)))
+            absolute = get_absolute_angle_in_degrees(robot_pos, point)
+            bearing = get_bearing_in_degrees(compass)
+            print(f"absolute: {absolute}, bearing: {bearing}")
+            print(f"relative: {math.fabs(absolute - bearing)}")
 
         left_motor.setVelocity(left_speed)
         right_motor.setVelocity(right_speed)
